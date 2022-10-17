6334e35450202d83841dd14b728fbd43f8b54b45
yxt <2251182973@qq.com>
2022-10-17 16:39:34 +0800

    实验0102 Spring校园卡充值

---
+++ exp0201/src/main/java/com/cqust/aspect/MyRight.java
@@ -0,0 +1,45 @@
+package com.cqust.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+
+@Aspect
+public class MyRight {
+
+    public void beforecheck(JoinPoint joinPoint) {
+        System.out.println(joinPoint.toString());
+        System.out.print("目标方法参数："+joinPoint.getArgs());
+        System.out.println("，目标对象："+joinPoint.getTarget());
+        System.out.println("before doing...");
+        System.out.println();
+    }
+
+    public void afterecheck(JoinPoint jp) {
+        System.out.println(jp.toString());
+        System.out.print("目标方法参数："+jp.getArgs());
+        System.out.println("，目标对象："+jp.getTarget());
+        System.out.println("after doing...");
+        System.out.println();
+    }
+
+    public Object aroundcheck(ProceedingJoinPoint point) throws Throwable{
+        System.out.println("环绕通知之前的部分");
+        Object object=point.proceed();
+        System.out.println("环绕通知结束的部分");
+        System.out.println();
+        return object;
+
+    }
+
+
+
+
+
+
+
+
+}
---
+++ exp0201/src/main/java/task1/demo02/AnnoAdvice.java
@@ -0,0 +1,42 @@
+package task1.demo02;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+
+@Aspect
+public class AnnoAdvice {
+    //切点
+    @Pointcut("execution(* task1.demo02.UserDaoImpl.*(..))")
+
+    public void pointcut(){
+    }
+
+    @Before("pointcut()")
+    public void before(JoinPoint joinPoint){
+        System.out.println("这是前置通知！");
+        System.out.print("目标类是："+joinPoint.getTarget());
+        System.out.println("，被织入增强处理的目标方法为："+joinPoint.getSignature().getName());
+    }
+    @AfterReturning("pointcut()")
+    public void afterReturing(JoinPoint jp) {
+        System.out.print("这是返回通知（方法不出现异常时调用）！");
+        System.out.println("被织入增强处理的目标方法为："+jp.getSignature().getName());
+    }
+    @Around("pointcut()")
+    public Object around(ProceedingJoinPoint point) throws Throwable {
+        System.out.println("这是环绕通知之前的部分！");
+        Object obj = point.proceed();
+        System.out.println("这是环绕通知之后的部分！");
+        return obj;
+    }
+    @AfterThrowing("pointcut()")
+    public void afterException(){
+        System.out.println("异常通知！");
+    }
+    @After("pointcut()")
+    public void after(){
+        System.out.println("这是后置通知！");
+    }
+
+}
