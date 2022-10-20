01873081e34010f606fc65f29d11767b127e63b4
yxt <2251182973@qq.com>
2022-10-20 11:26:47 +0800

    设置return 1

---
+++ chapter09/CS_JDBCTemplate/src/main/java/com/cqust/Dao/CardDao.java
@@ -0,0 +1,73 @@
+package com.cqust.Dao;
+import com.cqust.Entity.Card;
+import com.cqust.Entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class CardDao {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public int insert(Card card){
+        String strSql="insert into card(sid,sno,name,cno,money,lastAccessTime)VALUE(?,?,?,?,?,?)";
+        Object[] obj = new Object[]{
+                card.getSid(),
+                card.getSno(),
+                card.getName(),
+                card.getCno(),
+                card.getMoney(),
+                card.getLastAccessTime()
+        };
+
+
+        int num=this.jdbcTemplate.update(strSql, obj);
+        return num;
+    }
+
+    public int update(Card card){
+        String strSql = "update card set cno=? where cid=?";
+        Object[] obj = new Object[] {
+                card.getCno(),
+                card.getCid()};
+        int num = this.jdbcTemplate.update(strSql, obj);
+        return num;
+    }
+
+    public int delete(int cid){
+        String strSql = "delete from card where cid=?";
+        int num = this.jdbcTemplate.update(strSql, cid);
+        return num;
+    }
+
+
+    //查询一个
+    public Card selectById(int cid){
+        String strSql="Select * from card where cid= ?";
+        RowMapper<Card> rm =new BeanPropertyRowMapper<Card>(Card.class);
+        return  this.jdbcTemplate.queryForObject(strSql,rm,cid);
+    }
+
+    //查询所有
+    public List<Card> selectAllCard(){
+        String strSql = "select * from card";
+        RowMapper<Card> rm = new BeanPropertyRowMapper<>(Card.class);
+        return this.jdbcTemplate.query(strSql, rm);
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ chapter09/CS_JDBCTemplate/src/main/java/com/cqust/Entity/Card.java
@@ -0,0 +1,81 @@
+package com.cqust.Entity;
+
+
+public class Card {
+    private int cid;
+    private int sid;
+    private String sno;
+    private String name;
+    private String cno;
+    private float money;
+    private String lastAccessTime;
+
+    public int getCid() {
+        return cid;
+    }
+
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+
+    public int getSid() {
+        return sid;
+    }
+
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+
+    public String getSno() {
+        return sno;
+    }
+
+    public void setSno(String sno) {
+        this.sno = sno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCno() {
+        return cno;
+    }
+
+    public void setCno(String cno) {
+        this.cno = cno;
+    }
+
+    public float getMoney() {
+        return money;
+    }
+
+    public void setMoney(float money) {
+        this.money = money;
+    }
+
+    public String getLastAccessTime() {
+        return lastAccessTime;
+    }
+
+    public void setLastAccessTime(String lastAccessTime) {
+        this.lastAccessTime = lastAccessTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "cid=" + cid +
+                ", sid=" + sid +
+                ", sno='" + sno + '\'' +
+                ", name='" + name + '\'' +
+                ", cno='" + cno + '\'' +
+                ", money=" + money +
+                ", lastAccessTime='" + lastAccessTime + '\'' +
+                '}';
+    }
+}
