cc2cfac4b537820126f511b50ab6f1cf5a5b3488
uvw-z <115357496+uvw-z@users.noreply.github.com>
2022-10-27 22:28:30 +0800

    Delete SpringStart_Sutdent directory

--- SpringStart_Sutdent/src/main/java/com/cqust/entity/Student.java
+++
@@ -1,7 +0,0 @@
-package com.cqust.entity;
-
-public class Student {
-    public void say(){
-        System.out.println("I am a student!");
-    }
-}
--- SpringStart_Sutdent/src/main/java/com/cqust/entity/Teacher.java
+++
@@ -1,7 +0,0 @@
-package com.cqust.entity;
-
-public class Teacher {
-    public void speak(){
-        System.out.println("I am a teacher!");
-    }
-}
